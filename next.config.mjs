780552093554b3f0d953963e0f7a9a0dff62a1d8
Deepika <2215004@nec.edu.in>
2025-07-11 09:29:05 +0530

    Allow remote images from madrasshoppe.com and i.pinimg.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.madrasshoppe.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
